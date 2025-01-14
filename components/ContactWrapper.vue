14160b9ec11aa9b0bbe565243b0ebe0a707f66bd
kavyakunder10 <kavya@compose.co.in>
2025-01-14 14:06:20 +0530

    update location in map

--- components/ContactWrapper.vue
+++ components/ContactWrapper.vue
@@ -83,7 +83,9 @@
                         </div>
                     </div>
                     <div class="col-12 mt-35">
-                        <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d65368563.61295087!2d-117.82461439612302!3d-0.2494932804537185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x65a81cae36eb8ff%3A0xa6342257f310534f!2sAtlantic%20Ocean!5e0!3m2!1sen!2sbd!4v1614241876365!5m2!1sen!2sbd"></iframe>
+                        <iframe 
+                        class="google-map"  
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57067174.13924448!2d41.842609402440026!3d15.500208774520445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4b011c10366c03%3A0x6ee40296fd2dbcc5!2sOngole%2C%20Andhra%20Pradesh!5e0!3m2!1sen!2sin!4v1736837991039!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
