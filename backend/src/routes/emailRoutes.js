aa2cbcc54c6c9f24e2c9d6f1692615132b65565f
kavyakunder10 <kavya@compose.co.in>
2025-01-28 18:10:44 +0530

    remove backend

--- backend/src/routes/emailRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {sendEmail}=require("../controllers/emailController.js")
-
-router.post("/send-email", sendEmail);
-
-module.exports = router;
